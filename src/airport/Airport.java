f49d1422ce3f50e5c0abf89136850a26bb48f9bc
jay <xw042543@gmail.com>
2026-06-28 15:22:35 +0800

    feat : implement airport infrastruture

--- src/airport/Airport.java
+++ src/airport/Airport.java
@@ -1,4 +1,27 @@
 package airport;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Airport {
+    private Runway Airport;
+    private FuelTruck fuelTruck;
+    private List<Gate> gates;
+    public Airport () {
+        runway = new Runway();
+        fuelTruck = new FuelTruck();
+        gates = new ArrayList<>();
+        for (int i = 1; i <= 3 ; i++){
+            gates.add(new Gate(i));
+        }
+    }
+    public List<Gate> gatGates(){
+        return gates;
+    }
+    public Runway getRunway(){
+        return runway;
+    }
+    public FuelTruck getFuelTruck(){
+        return fuelTruck;
+    }
 }
--- src/airport/Gate.java
+++ src/airport/Gate.java
@@ -1,4 +1,29 @@
 package airport;
 
+import aircraft.Plane;
+
 public class Gate {
+    // Gate ID
+    private final int gateId;
+    // Plane currently occupying the gate
+    private Plane currentPlane;
+    // Constructor
+    public Gate(int gateId){
+        this.gateId = gateId;
+    }
+    public boolean isAvailable() {
+        return currentPlane == null;
+    }
+    public int getGateId() {
+        return gateId;
+    }
+    public void assignPlane(Plane plane) {
+        currentPlane =plane;
+    }
+    public void releasePlane() {
+        currentPlane = null;
+    }
+    public Plane getCurrentPlane() {
+        return currentPlane;
+    }
 }
